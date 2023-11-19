9a33c53166081fce913c8b5a39aa146edc3f10b8
Astemir Gurizhev <gurizhev.astema@mail.ru>
2023-11-19 14:55:47 +0300

    add burger and change header all html

--- js/script.js
+++ js/script.js
@@ -1,34 +1,41 @@
-"use strict"
+// "use strict"
 
-//elem.innerHTML="What of the fucker maker showcontribute"
-let elem = document.querySelector(".ewq p")
+// //elem.innerHTML="What of the fucker maker showcontribute"
+// let elem = document.querySelector(".ewq p")
 
-  //вешаем на него события
-elem.onmouseout = function() {
-    elem.style.color='';
-  }
+//   //вешаем на него события
+// elem.onmouseout = function() {
+//     elem.style.color='';
+//   }
 
-  elem.onmouseover = function() {
-    elem.style.color='yellow';
-  };
+//   elem.onmouseover = function() {
+//     elem.style.color='yellow';
+//   };
 
-window.onload = function () {
+// window.onload = function () {
 
-    //получаем идентификатор элемента
+//     //получаем идентификатор элемента
     
-    //вешаем на него событие
+//     //вешаем на него событие
     
-    elem.onclick = function() {
-        //производим какие-то действия
+//     elem.onclick = function() {
+//         //производим какие-то действия
                 
-        if (this.innerHTML=='We hired people who are <br> Always Passionate about <br> what they do') 
-            elem.innerHTML="What of the fucker maker showcontribute - click me please"
-        else
-            elem.innerHTML="We hired people who are <br> Always Passionate about <br> what they do"
+//         if (this.innerHTML=='We hired people who are <br> Always Passionate about <br> what they do') 
+//             elem.innerHTML="What of the fucker maker showcontribute - click me please"
+//         else
+//             elem.innerHTML="We hired people who are <br> Always Passionate about <br> what they do"
 
-            //предотвращаем переход по ссылке href
-        return false;
-    }
+//             //предотвращаем переход по ссылке href
+//         return false;
+//     }
     
     
-}
+// }
+
+let burger = document.querySelector('.burger')
+let headerMenuBurger = document.querySelector('.header-menu__burger')
+
+burger.addEventListener('click', () => {
+  headerMenuBurger.classList.toggle('header-menu-burger__active')
+})
