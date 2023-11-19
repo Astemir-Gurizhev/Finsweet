294092d7f7b78ac73ec3f8ce138913da43459736
RedeemerRid <akhmatovich@gmail.com>
2023-11-19 10:21:17 +0300

    home js burger 02

--- js/careers.js
+++ js/careers.js
@@ -1,10 +1,16 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.header-menu-togle');
+let overlay = document.querySelector('.overlay')
 menuBtn.addEventListener('click', function() {
-        menuBtn.classList.toggle('active');
-        menu.classList.toggle('active');
-    })
-    // menuBtn.addEventListener('mouseover', function() {
-    //     menuBtn.classList.toggle('active');
-    //     menu.classList.toggle('hmt-hidden');
-    // })
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+    overlay.classList.toggle('hidden')
+
+})
+
+overlay.addEventListener('click', function() {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+    overlay.classList.toggle('hidden')
+
+})
